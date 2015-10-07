2b47f4420746243ada19c876b30d0b8be3fb0873
jenkins1967 <jenkins2@gmail.com>
2015-10-07 18:55:57 -0400

    added the file back in

---
+++ JimJenkins.GeoCoding.Services/IGeoCodingServiceConfigProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace JimJenkins.GeoCoding.Services
+{
+    public interface IGeoCodingServiceConfigProvider
+    {
+        Uri BaseUri { get; set; }
+        
+    }
+
+    public class GeoCodingServiceConfigProvider : IGeoCodingServiceConfigProvider
+    {
+        public GeoCodingServiceConfigProvider(string serviceKey)
+        {
+            if (string.IsNullOrEmpty(serviceKey))
+            {
+                throw new ArgumentNullException("serviceKey");
+            }
+            BaseUri =
+                new Uri("https://maps.googleapis.com/maps/api/geocode/json?key=" + serviceKey);
+
+     
+        }
+        public Uri BaseUri { get; set; }
+        
+    }
+}
