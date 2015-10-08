cc31f12ad92c04009d7c1fbf5db4b8ae31c283c9
Jim Jenkins <jenkins2@gmail.com>
2015-10-08 08:30:49 -0400

    fixed problem with merge of file

--- JimJenkins.GeoCoding.Services/IGeoCodingServiceConfigProvider.cs
+++ JimJenkins.GeoCoding.Services/IGeoCodingServiceConfigProvider.cs
@@ -19,12 +19,6 @@ namespace JimJenkins.GeoCoding.Services
             BaseUri =
                 new Uri("https://maps.googleapis.com/maps/api/geocode/json?key=" + serviceKey);
 
-<<<<<<< HEAD
-     
-=======
-        //https://maps.googleapis.com/maps/api/geocode/json?address=19055&key=AIzaSyDrDkd4cYr21RpL0eR8V6SvQOr-19y4BBo
-      //      ApiKey = "AIzaSyDrDkd4cYr21RpL0eR8V6SvQOr-19y4BBo;";
->>>>>>> origin/master
         }
         public Uri BaseUri { get; set; }
         
